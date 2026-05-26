6cf221080c758f0c1d047186257c05f907ee1592
asesh <abc@gmail.com>
2026-05-27 01:16:39 +0530

    Fix PostgreSQL template meta-commands

--- platform-infra/prod-debian/postgres/create-dbs.sql
+++ platform-infra/prod-debian/postgres/create-dbs.sql
@@ -1,10 +1,11 @@
-\
-set ON_ERROR_STOP on -- Required psql variables:
-    --   auth_db_name, auth_db_user,
-    --   radio_db_name, radio_db_user,
-    --   curio_db_name, curio_db_user,
-    --   seeksage_db_name, seeksage_db_user,
-    --   notestack_db_name, notestack_db_user
+\set ON_ERROR_STOP on
+
+-- Required psql variables:
+--   auth_db_name, auth_db_user,
+--   radio_db_name, radio_db_user,
+--   curio_db_name, curio_db_user,
+--   seeksage_db_name, seeksage_db_user,
+--   notestack_db_name, notestack_db_user
 SELECT format(
         'CREATE DATABASE %I OWNER %I ENCODING ''UTF8''',
         :'auth_db_name',
@@ -14,7 +15,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_database
         WHERE datname = :'auth_db_name'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'CREATE DATABASE %I OWNER %I ENCODING ''UTF8''',
         :'radio_db_name',
@@ -24,7 +25,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_database
         WHERE datname = :'radio_db_name'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'CREATE DATABASE %I OWNER %I ENCODING ''UTF8''',
         :'curio_db_name',
@@ -34,7 +35,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_database
         WHERE datname = :'curio_db_name'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'CREATE DATABASE %I OWNER %I ENCODING ''UTF8''',
         :'seeksage_db_name',
@@ -44,7 +45,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_database
         WHERE datname = :'seeksage_db_name'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'CREATE DATABASE %I OWNER %I ENCODING ''UTF8''',
         :'notestack_db_name',
@@ -54,29 +55,29 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_database
         WHERE datname = :'notestack_db_name'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'GRANT ALL PRIVILEGES ON DATABASE %I TO %I',
         :'auth_db_name',
         :'auth_db_user'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'GRANT ALL PRIVILEGES ON DATABASE %I TO %I',
         :'radio_db_name',
         :'radio_db_user'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'GRANT ALL PRIVILEGES ON DATABASE %I TO %I',
         :'curio_db_name',
         :'curio_db_user'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'GRANT ALL PRIVILEGES ON DATABASE %I TO %I',
         :'seeksage_db_name',
         :'seeksage_db_user'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'GRANT ALL PRIVILEGES ON DATABASE %I TO %I',
         :'notestack_db_name',
         :'notestack_db_user'
-    ) \ gexec
+    ) \gexec
--- platform-infra/prod-debian/postgres/users-and-permissions.sql
+++ platform-infra/prod-debian/postgres/users-and-permissions.sql
@@ -1,10 +1,11 @@
-\
-set ON_ERROR_STOP on -- Required psql variables:
-    --   auth_db_user, auth_db_password,
-    --   radio_db_user, radio_db_password,
-    --   curio_db_user, curio_db_password,
-    --   seeksage_db_user, seeksage_db_password,
-    --   notestack_db_user, notestack_db_password
+\set ON_ERROR_STOP on
+
+-- Required psql variables:
+--   auth_db_user, auth_db_password,
+--   radio_db_user, radio_db_password,
+--   curio_db_user, curio_db_password,
+--   seeksage_db_user, seeksage_db_password,
+--   notestack_db_user, notestack_db_password
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'auth_db_user',
@@ -14,7 +15,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'auth_db_user'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'radio_db_user',
@@ -24,7 +25,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'radio_db_user'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'curio_db_user',
@@ -34,7 +35,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'curio_db_user'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'seeksage_db_user',
@@ -44,7 +45,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'seeksage_db_user'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'notestack_db_user',
@@ -54,29 +55,31 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'notestack_db_user'
-    ) \ gexec -- Always keep role passwords aligned with deployment configuration.
+    ) \gexec
+
+-- Always keep role passwords aligned with deployment configuration.
 SELECT format(
         'ALTER ROLE %I WITH LOGIN PASSWORD %L',
         :'auth_db_user',
         :'auth_db_password'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'ALTER ROLE %I WITH LOGIN PASSWORD %L',
         :'radio_db_user',
         :'radio_db_password'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'ALTER ROLE %I WITH LOGIN PASSWORD %L',
         :'curio_db_user',
         :'curio_db_password'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'ALTER ROLE %I WITH LOGIN PASSWORD %L',
         :'seeksage_db_user',
         :'seeksage_db_password'
-    ) \ gexec
+    ) \gexec
 SELECT format(
         'ALTER ROLE %I WITH LOGIN PASSWORD %L',
         :'notestack_db_user',
         :'notestack_db_password'
-    ) \ gexec
+    ) \gexec
