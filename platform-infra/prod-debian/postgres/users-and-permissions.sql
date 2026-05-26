7d076a61e95ad57f7c2fbe0673d85484bdbf5251
asesh <abc@gmail.com>
2026-05-26 23:32:24 +0530

    postgresql....

--- platform-infra/prod-debian/postgres/users-and-permissions.sql
+++ platform-infra/prod-debian/postgres/users-and-permissions.sql
@@ -1,5 +1,5 @@
-\set ON_ERROR_STOP on
-    --   auth_db_user, auth_db_password,
+\
+set ON_ERROR_STOP on --   auth_db_user, auth_db_password,
     --   radio_db_user, radio_db_password,
     --   curio_db_user, curio_db_password,
     --   seeksage_db_user, seeksage_db_password,
@@ -13,7 +13,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'auth_db_user'
-    ) \gexec
+    ) \ gexec
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'radio_db_user',
@@ -23,7 +23,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'radio_db_user'
-    ) \gexec
+    ) \ gexec
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'curio_db_user',
@@ -33,7 +33,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'curio_db_user'
-    ) \gexec
+    ) \ gexec
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'seeksage_db_user',
@@ -43,8 +43,7 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'seeksage_db_user'
-    ) \gexec
-
+    ) \ gexec
 SELECT format(
         'CREATE ROLE %I LOGIN PASSWORD %L',
         :'notestack_db_user',
@@ -54,11 +53,29 @@ WHERE NOT EXISTS (
         SELECT 1
         FROM pg_roles
         WHERE rolname = :'notestack_db_user'
-    ) \gexec
-
--- Always keep role passwords aligned with deployment configuration.
-SELECT format('ALTER ROLE %I WITH LOGIN PASSWORD %L', :'auth_db_user', :'auth_db_password') \gexec
-SELECT format('ALTER ROLE %I WITH LOGIN PASSWORD %L', :'radio_db_user', :'radio_db_password') \gexec
-SELECT format('ALTER ROLE %I WITH LOGIN PASSWORD %L', :'curio_db_user', :'curio_db_password') \gexec
-SELECT format('ALTER ROLE %I WITH LOGIN PASSWORD %L', :'seeksage_db_user', :'seeksage_db_password') \gexec
-SELECT format('ALTER ROLE %I WITH LOGIN PASSWORD %L', :'notestack_db_user', :'notestack_db_password') \gexec
+    ) \ gexec -- Always keep role passwords aligned with deployment configuration.
+SELECT format(
+        'ALTER ROLE %I WITH LOGIN PASSWORD %L',
+        :'auth_db_user',
+        :'auth_db_password'
+    ) \ gexec
+SELECT format(
+        'ALTER ROLE %I WITH LOGIN PASSWORD %L',
+        :'radio_db_user',
+        :'radio_db_password'
+    ) \ gexec
+SELECT format(
+        'ALTER ROLE %I WITH LOGIN PASSWORD %L',
+        :'curio_db_user',
+        :'curio_db_password'
+    ) \ gexec
+SELECT format(
+        'ALTER ROLE %I WITH LOGIN PASSWORD %L',
+        :'seeksage_db_user',
+        :'seeksage_db_password'
+    ) \ gexec
+SELECT format(
+        'ALTER ROLE %I WITH LOGIN PASSWORD %L',
+        :'notestack_db_user',
+        :'notestack_db_password'
+    ) \ gexec
